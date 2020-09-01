e8669f813676645e8fe773cf73af06fcddea7efd
Pak Artem <obscurexdlol@gmail.com>
2020-09-01 18:02:15 +0300

    initial commit

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,43 @@
+<template>
+  <form @submit.prevent="onSubmit">
+    <input type="text" v-model="title">
+    <button type="submit">Create</button>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: ''
+    }
+  },
+  methods: {
+    onSubmit(){
+      console.log(this.title);
+      if(this.title.trim()) {
+        console.log('submit');
+        const newTodo = {
+          id: Date.now(),
+          title: this.title,
+          completed: false
+        }
+
+        this.$emit('add-todo', newTodo)
+        this.title = ''
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+  form {
+    display: flex;
+    margin: 2rem;
+  }
+
+  input {
+    width: 400px;
+  }
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <h2>Home page</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum veniam libero similique iste possimus illo atque autem ullam, soluta odio asperiores nisi sit porro sunt assumenda eligendi corrupti amet dolore!</p>
+
+    <router-link to="/todos">Todos</router-link>
+  </div>
+</template>
